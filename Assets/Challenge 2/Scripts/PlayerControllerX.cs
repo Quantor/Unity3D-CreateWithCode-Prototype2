211c75a9eb5aa316d8bec9e6382b2f98b8625257
Quantor <jensch.oliver@web.de>
2021-08-08 19:23:59 +0200

    Init

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float allowedSpawnInterval = 1;
+    //public float timeOfLastSpawnedDog = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        allowedSpawnInterval -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && allowedSpawnInterval <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            allowedSpawnInterval = 1;
+        }
+    }
+}
